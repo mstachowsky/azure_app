9fcf32c883809f9d771404be9d7af0dfe6530005
Mike Cooper-Stachowsky <mstachow@uwaterloo.ca>
2024-02-18 09:27:36 -0500

    adding openai backdf

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -16,6 +16,7 @@ def hello(request):
             print("Request for hello page received with no name or blank name -- redirecting")
             return redirect('index')
         else:
+            openai.api_key = process.env.OPENAI_API_KEY#settings.OPENAI_API_KEY
             print("Request for hello page received with name=%s" % name)
             context = {'name': name }
             return render(request, 'hello_azure/hello.html', context)
