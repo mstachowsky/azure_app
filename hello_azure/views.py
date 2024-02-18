4fba3862c6ccee743fe800b63356025e5ec574ac
Mike Cooper-Stachowsky <mstachow@uwaterloo.ca>
2024-02-18 09:08:38 -0500

    revert

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -1,3 +1,27 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+def index(request):
+    print('Request for index page received')
+    return render(request, 'hello_azure/index.html')
+
+@csrf_exempt
+def hello(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        
+        if name is None or name == '':
+            print("Request for hello page received with no name or blank name -- redirecting")
+            return redirect('index')
+        else:
+            print("Request for hello page received with name=%s" % name)
+            context = {'name': name }
+            return render(request, 'hello_azure/hello.html', context)
+    else:
+        return redirect('index')
+
+"""
 import openai
 from django.conf import settings
 
@@ -21,13 +45,13 @@ def hello(request):
             #openai.api_key = process.env.OPENAI_API_KEY#settings.OPENAI_API_KEY
 
             # Replace "text-davinci-002" with the model you want to use
-            """
+            
             response = openai.Completion.create(
               engine="gpt-3.5-turbo",
               prompt=f"Say hello to {name}",
               max_tokens=50
             )
-            """
+            
 
             # Extract the response text
             message = "Cheese"#response.choices[0].text.strip()
@@ -38,3 +62,4 @@ def hello(request):
             return render(request, 'templates/hello_azure/hello.html', context)
     else:
         return redirect('index')
+"""
