ab8e30bd9008b136d694cc8b617a624a66363d1f
Mike Cooper-Stachowsky <mstachow@uwaterloo.ca>
2024-02-18 09:46:42 -0500

    new openai api

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-import openai
+from openai import OpenAI
 import os
 import traceback
+
+def get_gpt_response(prompt):
+    # gets API Key from environment variable OPENAI_API_KEY
+    client = OpenAI()
+
+    # Non-streaming:
+    print("----- standard request -----")
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+    )
+    print(completion.choices[0].message.content)
+    return completion.choices[0].message.content
 def index(request):
     print('Request for index page received')
     return render(request, 'hello_azure/index.html')
@@ -17,15 +35,15 @@ def hello(request):
             print("Request for hello page received with no name or blank name -- redirecting")
             return redirect('index')
         else:
-            openai.api_key = os.environ['OPENAI_API_KEY']
+            #openai.api_key = os.environ['OPENAI_API_KEY']
            
             prompt=f"Say hello to {name}",
             #settings.OPENAI_API_KEY
             message = 'fail'
             try:
-                response = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=[{"role":"user","content":prompt}],temperature=0.1)
-            
-                message = response.choices[0].text.strip()
+                #response = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=[{"role":"user","content":prompt}],temperature=0.1)
+                
+                message = get_gpt_response(prompt)#response.choices[0].text.strip()
                 if not message:
                     message = response
 
