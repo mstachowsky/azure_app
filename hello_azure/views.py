9237cb7005347cdb96dc3a492096ba5027b935dc
Mike Cooper-Stachowsky <mstachow@uwaterloo.ca>
2024-03-07 07:33:55 -0500

    read file

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -44,6 +44,19 @@ def list_blobs_in_container(container_name):
     blob_names = [blob.name for blob in container_client.list_blobs()]
     return blob_names
 
+def get_blob_content(container_name, blob_name):
+    # Fetch the connection string from an environment variable
+    connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+
+    # Instantiate a BlobServiceClient using the connection string
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+    # Instantiate a BlobClient
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+    # Download the blob's contents and return as a string
+    blob_content = blob_client.download_blob().readall()
+    return blob_content.decode('utf-8')
 
 @csrf_exempt
 def hello(request):
@@ -63,7 +76,7 @@ def hello(request):
                 #response = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=[{"role":"user","content":prompt}],temperature=0.1)
                 
                 print(list_blobs_in_container("rubrics"))
-                
+                print(get_blob_content("rubrics",list_blobs_in_container("rubrics")[0]))
                 message = get_gpt_response(prompt) + "".join(list_blobs_in_container("rubrics")) + "files"#response.choices[0].text.strip()
 
                 context = {'name': name, 'message': message}
